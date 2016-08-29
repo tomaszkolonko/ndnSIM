34ace6e2b98455cca6b73428ee2b14b825edca08
Tomasz Kolonko <tz.kolonko@gmail.com>
2016-08-29 12:33:01 +0200

    added new scenarios

---
+++ examples/ndn-simple-wifi-with-lte.cpp
@@ -0,0 +1,286 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/internet-module.h"
+
+#include "ns3/ndnSIM-module.h"
+
+// LTE imports
+#include "ns3/lte-helper.h"
+#include "ns3/epc-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/internet-module.h"
+#include "ns3/lte-module.h"
+#include "ns3/point-to-point-helper.h"
+#include "ns3/config-store.h"
+
+using namespace std;
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE("ndn.WifiExample");
+
+//
+// DISCLAIMER:  Note that this is an extremely simple example, containing just 2 wifi nodes
+// communicating directly over AdHoc channel.
+//
+
+// Ptr<ndn::NetDeviceFace>
+// MyNetDeviceFaceCallback (Ptr<Node> node, Ptr<ndn::L3Protocol> ndn, Ptr<NetDevice> device)
+// {
+//   // NS_LOG_DEBUG ("Create custom network device " << node->GetId ());
+//   Ptr<ndn::NetDeviceFace> face = CreateObject<ndn::MyNetDeviceFace> (node, device);
+//   ndn->AddFace (face);
+//   return face;
+// }
+
+int
+main(int argc, char* argv[])
+{
+
+	// LTE VARIABLES >>>>
+	  uint16_t numberOfNodes = 2;
+	  double simTime = 1.1;
+	  double distance = 60.0;
+	  double interPacketInterval = 100;
+	// LTE VARIABLES <<<<
+
+  // disable fragmentation
+  Config::SetDefault("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue("2200"));
+  Config::SetDefault("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue("2200"));
+  Config::SetDefault("ns3::WifiRemoteStationManager::NonUnicastMode",
+                     StringValue("OfdmRate24Mbps"));
+
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
+
+  //////////////////////
+  WifiHelper wifi = WifiHelper::Default();
+  // wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+  wifi.SetStandard(WIFI_PHY_STANDARD_80211a);
+  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager", "DataMode",
+                               StringValue("OfdmRate24Mbps"));
+
+  YansWifiChannelHelper wifiChannel; // = YansWifiChannelHelper::Default ();
+  wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss("ns3::ThreeLogDistancePropagationLossModel");
+  wifiChannel.AddPropagationLoss("ns3::NakagamiPropagationLossModel");
+
+  // YansWifiPhy wifiPhy = YansWifiPhy::Default();
+  YansWifiPhyHelper wifiPhyHelper = YansWifiPhyHelper::Default();
+  wifiPhyHelper.SetChannel(wifiChannel.Create());
+  wifiPhyHelper.Set("TxPowerStart", DoubleValue(5));
+  wifiPhyHelper.Set("TxPowerEnd", DoubleValue(5));
+
+  NqosWifiMacHelper wifiMacHelper = NqosWifiMacHelper::Default();
+  wifiMacHelper.SetType("ns3::AdhocWifiMac");
+
+  Ptr<UniformRandomVariable> randomizer = CreateObject<UniformRandomVariable>();
+  randomizer->SetAttribute("Min", DoubleValue(10));
+  randomizer->SetAttribute("Max", DoubleValue(100));
+
+  MobilityHelper mobility;
+  mobility.SetPositionAllocator("ns3::RandomBoxPositionAllocator", "X", PointerValue(randomizer),
+                                "Y", PointerValue(randomizer), "Z", PointerValue(randomizer));
+
+  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+
+  NodeContainer nodes;
+  nodes.Create(2);
+
+  ////////////////
+  // 1. Install Wifi
+  NetDeviceContainer wifiNetDevices = wifi.Install(wifiPhyHelper, wifiMacHelper, nodes);
+
+  // 2. Install Mobility model
+  mobility.Install(nodes);
+
+  // 3. Install NDN stack
+  NS_LOG_INFO("Installing NDN stack");
+  ndn::StackHelper ndnHelper;
+  // ndnHelper.AddNetDeviceFaceCreateCallback (WifiNetDevice::GetTypeId (), MakeCallback
+  // (MyNetDeviceFaceCallback));
+  ndnHelper.SetOldContentStore("ns3::ndn::cs::Lru", "MaxSize", "1000");
+  ndnHelper.SetDefaultRoutes(true);
+  ndnHelper.Install(nodes);
+
+  // Set BestRoute strategy
+  ndn::StrategyChoiceHelper::Install(nodes, "/", "/localhost/nfd/strategy/best-route");
+
+  // 4. Set up applications
+  NS_LOG_INFO("Installing Applications");
+
+  ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
+  consumerHelper.SetPrefix("/test/prefix");
+  consumerHelper.SetAttribute("Frequency", DoubleValue(10.0));
+  consumerHelper.Install(nodes.Get(0));
+
+  ndn::AppHelper producerHelper("ns3::ndn::Producer");
+  producerHelper.SetPrefix("/");
+  producerHelper.SetAttribute("PayloadSize", StringValue("1200"));
+  producerHelper.Install(nodes.Get(1));
+
+  ////////////////
+
+  // LTE SCRIPT START >>>>
+
+
+    cmd.AddValue("numberOfNodes", "Number of eNodeBs + UE pairs", numberOfNodes);
+    cmd.AddValue("simTime", "Total duration of the simulation [s])", simTime);
+    cmd.AddValue("distance", "Distance between eNBs [m]", distance);
+    cmd.AddValue("interPacketInterval", "Inter packet interval [ms])", interPacketInterval);
+    cmd.Parse(argc, argv);
+
+    Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+
+    // TODO: this is a problem since ipv4
+    Ptr<PointToPointEpcHelper>  epcHelper = CreateObject<PointToPointEpcHelper> ();
+
+    lteHelper->SetEpcHelper (epcHelper);
+
+    ConfigStore inputConfig;
+    inputConfig.ConfigureDefaults();
+
+    // parse again so you can override default values from the command line
+    cmd.Parse(argc, argv);
+
+    Ptr<Node> pgw = epcHelper->GetPgwNode ();
+
+     // Create a single RemoteHost
+    NodeContainer remoteHostContainer;
+    remoteHostContainer.Create (1);
+    Ptr<Node> remoteHost = remoteHostContainer.Get (0);
+    InternetStackHelper internet;
+    internet.Install (remoteHostContainer);
+
+    // Create the Internet
+    PointToPointHelper p2ph;
+    p2ph.SetDeviceAttribute ("DataRate", DataRateValue (DataRate ("100Gb/s")));
+    p2ph.SetDeviceAttribute ("Mtu", UintegerValue (1500));
+    p2ph.SetChannelAttribute ("Delay", TimeValue (Seconds (0.010)));
+    NetDeviceContainer internetDevices = p2ph.Install (pgw, remoteHost);
+    Ipv4AddressHelper ipv4h;
+    ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
+    Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
+    // interface 0 is localhost, 1 is the p2p device
+    Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
+
+    Ipv4StaticRoutingHelper ipv4RoutingHelper;
+    Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
+    remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
+
+    NodeContainer ueNodes;
+    NodeContainer enbNodes;
+    enbNodes.Create(numberOfNodes);
+    ueNodes.Create(numberOfNodes);
+
+
+
+    // Install LTE Devices to the nodes
+    NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice (enbNodes);
+    NetDeviceContainer ueLteDevs = lteHelper->InstallUeDevice (ueNodes);
+
+    // Install the IP stack on the UEs
+    internet.Install (ueNodes);
+    Ipv4InterfaceContainer ueIpIface;
+    ueIpIface = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueLteDevs));
+    // Assign IP address to UEs, and install applications
+    for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
+      {
+        Ptr<Node> ueNode = ueNodes.Get (u);
+        // Set the default gateway for the UE
+        Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting (ueNode->GetObject<Ipv4> ());
+        ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
+      }
+
+    // Attach one UE per eNodeB
+    for (uint16_t i = 0; i < numberOfNodes; i++)
+        {
+          lteHelper->Attach (ueLteDevs.Get(i), enbLteDevs.Get(i));
+          // side effect: the default EPS bearer will be activated
+        }
+
+
+
+    // Install and start applications on UEs and remote host
+    uint16_t dlPort = 1234;
+    uint16_t ulPort = 2000;
+    uint16_t otherPort = 3000;
+    ApplicationContainer clientApps;
+    ApplicationContainer serverApps;
+    for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
+      {
+        ++ulPort;
+        ++otherPort;
+        PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
+        PacketSinkHelper ulPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
+        PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
+        serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
+        serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
+        serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
+
+        UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort);
+        dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
+        dlClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
+
+        UdpClientHelper ulClient (remoteHostAddr, ulPort);
+        ulClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
+        ulClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
+
+        UdpClientHelper client (ueIpIface.GetAddress (u), otherPort);
+        client.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
+        client.SetAttribute ("MaxPackets", UintegerValue(1000000));
+
+        clientApps.Add (dlClient.Install (remoteHost));
+        clientApps.Add (ulClient.Install (ueNodes.Get(u)));
+        if (u+1 < ueNodes.GetN ())
+          {
+            clientApps.Add (client.Install (ueNodes.Get(u+1)));
+          }
+        else
+          {
+            clientApps.Add (client.Install (ueNodes.Get(0)));
+          }
+      }
+    serverApps.Start (Seconds (0.01));
+    clientApps.Start (Seconds (0.01));
+    lteHelper->EnableTraces ();
+
+
+  // LTE SCRIPT STOP <<<<
+
+  Simulator::Stop(Seconds(30.0));
+
+  Simulator::Run();
+  Simulator::Destroy();
+
+  return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
+}
---
+++ examples/ndn-simple-wifi-with-mobility-tz1.cpp
@@ -0,0 +1,200 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/ndnSIM/utils/tracers/ndn-l3-rate-tracer.hpp"
+
+#include "ns3/ndnSIM-module.h"
+
+using namespace std;
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE("ndn.WifiExample");
+
+//
+// DISCLAIMER:  Note that this is an extremely simple example, containing just 2 wifi nodes
+// communicating directly over AdHoc channel.
+//
+
+// Ptr<ndn::NetDeviceFace>
+// MyNetDeviceFaceCallback (Ptr<Node> node, Ptr<ndn::L3Protocol> ndn, Ptr<NetDevice> device)
+// {
+//   // NS_LOG_DEBUG ("Create custom network device " << node->GetId ());
+//   Ptr<ndn::NetDeviceFace> face = CreateObject<ndn::MyNetDeviceFace> (node, device);
+//   ndn->AddFace (face);
+//   return face;
+// }
+
+void showPosition (Ptr<Node> node, double deltaTime)
+{
+  uint32_t nodeId = node->GetId ();
+  Ptr<MobilityModel> mobModel = node->GetObject<MobilityModel> ();
+  Vector3D pos = mobModel->GetPosition ();
+  Vector3D speed = mobModel->GetVelocity ();
+  std::cout << "At " << Simulator::Now ().GetSeconds () << " node " << nodeId
+            << ": Position(" << pos.x << ", " << pos.y << ", " << pos.z
+            << ");   Speed(" << speed.x << ", " << speed.y << ", " << speed.z
+            << ")" << std::endl;
+
+  Simulator::Schedule (Seconds (deltaTime), &showPosition, node, deltaTime);
+}
+
+
+int
+main(int argc, char* argv[])
+{
+
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+  // disable fragmentation
+  Config::SetDefault("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue("2200"));
+  Config::SetDefault("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue("2200"));
+  Config::SetDefault("ns3::WifiRemoteStationManager::NonUnicastMode",
+                     StringValue("OfdmRate24Mbps"));
+
+  std::cout.precision (2);
+  std::cout.setf (std::ios::fixed);
+  int nodeNum = 5;
+
+  double deltaTime = 10;
+  std::string traceFile1 = "src/ndnSIM/examples/trace-files/ndn-simple-wifi-tracefile1";
+  std::string traceFile2 = "src/ndnSIM/examples/trace-files/ndn-simple-wifi-tracefile2";
+  std::string traceFile3 = "src/ndnSIM/examples/trace-files/ndn-simple-wifi-tracefile3";
+
+  std::string currentTraceFile = traceFile3;
+
+  CommandLine cmd;
+  cmd.AddValue ("traceFile", "Ns2 movement trace file", currentTraceFile);
+  cmd.AddValue ("deltaTime", "time interval (s) between updates (default 100)", deltaTime);
+  // apparently that must be provided with same number of nodes as in tracefile
+  cmd.AddValue ("nodeNum", "Number of nodes", nodeNum);
+  cmd.Parse(argc, argv);
+
+  //////////////////////
+  WifiHelper wifi = WifiHelper::Default();
+  // wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+  wifi.SetStandard(WIFI_PHY_STANDARD_80211a);
+  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager", "DataMode",
+                               StringValue("OfdmRate24Mbps"));
+
+  YansWifiChannelHelper wifiChannel; // = YansWifiChannelHelper::Default ();
+  wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss("ns3::ThreeLogDistancePropagationLossModel");
+  wifiChannel.AddPropagationLoss("ns3::NakagamiPropagationLossModel");
+
+  // YansWifiPhy wifiPhy = YansWifiPhy::Default();
+  YansWifiPhyHelper wifiPhyHelper = YansWifiPhyHelper::Default();
+  wifiPhyHelper.SetChannel(wifiChannel.Create());
+  wifiPhyHelper.Set("TxPowerStart", DoubleValue(5));
+  wifiPhyHelper.Set("TxPowerEnd", DoubleValue(5));
+
+  NqosWifiMacHelper wifiMacHelper = NqosWifiMacHelper::Default();
+  wifiMacHelper.SetType("ns3::AdhocWifiMac");
+
+  Ptr<Node> n0 = CreateObject<Node> ();
+  Ptr<Node> n1 = CreateObject<Node> ();
+  Ptr<Node> n2 = CreateObject<Node> ();
+  Ptr<Node> n3 = CreateObject<Node> ();
+  Ptr<Node> n4 = CreateObject<Node> ();
+
+  NodeContainer nodes;
+  nodes.Add(n0);
+  nodes.Add(n1);
+  nodes.Add(n2);
+  nodes.Add(n3);
+  nodes.Add(n4);
+
+  std::cout << "**********************************************" << std::endl;
+  std::cout << "node(0) has " << (*nodes.Get(0)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(1) has " << (*nodes.Get(1)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(2) has " << (*nodes.Get(2)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(3) has " << (*nodes.Get(3)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(4) has " << (*nodes.Get(4)).GetNDevices() << " faces." << std::endl;
+  std::cout << "**********************************************" << std::endl;
+
+
+  ////////////////
+  ///////////////
+  // 1. Install Wifi
+  NetDeviceContainer wifiNetDevices = wifi.Install(wifiPhyHelper, wifiMacHelper, nodes);
+
+  // 2. Install Mobility model
+  //mobility.Install(nodes);
+
+  	  Ns2MobilityHelper ns2 = Ns2MobilityHelper (currentTraceFile);
+  	  ns2.Install ();
+
+  // 3. Install NDN stack
+  NS_LOG_INFO("Installing NDN stack");
+  ndn::StackHelper ndnHelper;
+  // ndnHelper.AddNetDeviceFaceCreateCallback (WifiNetDevice::GetTypeId (), MakeCallback
+  // (MyNetDeviceFaceCallback));
+  ndnHelper.SetOldContentStore("ns3::ndn::cs::Lru", "MaxSize", "1000");
+  ndnHelper.SetDefaultRoutes(true);
+  ndnHelper.Install(nodes);
+
+  // Set BestRoute strategy
+  ndn::StrategyChoiceHelper::Install(nodes, "/", "/localhost/nfd/strategy/multicast-strategy");
+
+  // 4. Set up applications
+  NS_LOG_INFO("Installing Applications");
+
+  ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
+  consumerHelper.SetPrefix("/test/prefix");
+  consumerHelper.SetAttribute("Frequency", DoubleValue(5.0));
+  consumerHelper.Install(nodes.Get(0));
+
+  ndn::AppHelper producerHelper("ns3::ndn::Producer");
+  producerHelper.SetPrefix("/");
+  producerHelper.SetAttribute("PayloadSize", StringValue("1200"));
+  producerHelper.Install(nodes.Get(4));
+
+  std::cout << "**********************************************" << std::endl;
+  std::cout << "node(0) has " << (*nodes.Get(0)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(1) has " << (*nodes.Get(1)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(2) has " << (*nodes.Get(2)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(3) has " << (*nodes.Get(3)).GetNDevices() << " faces." << std::endl;
+  std::cout << "node(4) has " << (*nodes.Get(4)).GetNDevices() << " faces." << std::endl;
+  std::cout << "**********************************************" << std::endl;
+
+  ////////////////
+
+  ndn::L3RateTracer::InstallAll("rate-trace.txt", Seconds(1));
+
+  Simulator::Stop(Seconds(3));
+
+  Simulator::Run();
+  Simulator::Destroy();
+
+  return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
+}
